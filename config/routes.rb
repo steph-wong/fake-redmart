61cf5e19a86a972773cf476816d9ba05209d5dcc
Stephanie Wong <stephw.yw@gmail.com>
2016-07-26 23:35:32 +0800

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/register', to: 'users#new'
+
 end
