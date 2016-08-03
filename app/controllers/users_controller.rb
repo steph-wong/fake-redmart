8ac769f4b65c049c266ba221e52078ad4919a3e8
Stephanie Wong <stephw.yw@gmail.com>
2016-08-03 23:49:33 +0800

    correct auto login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        @current_user = user
+        log_in(@user)
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
