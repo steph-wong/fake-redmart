921907e9dba5e568580fad65b99316f2d9e9061d
Stephanie Wong <stephw.yw@gmail.com>
2016-08-03 23:34:42 +0800

    user login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        @current_user = user
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
